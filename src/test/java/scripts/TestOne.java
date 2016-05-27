7ceda6411422cb26d5d2ba4e665465a363bf95ca
Sagar Gandiboiena <sagar.gandiboiena@zenq.com>
2016-05-27 12:25:14 +0530

    First Commit

---
+++ src/test/java/scripts/TestOne.java
@@ -0,0 +1,12 @@
+package scripts;
+
+/**
+ * Created by sagar.gandiboiena on 5/27/2016.
+ */
+public class TestOne {
+
+    public static void main(){
+        System.out.println("Hello World!");
+    }
+
+}
